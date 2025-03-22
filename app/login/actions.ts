a81466f9702d58b6a11de51dcd9842564542672c
RodrLucas <lucasrodriguessantos0@gmail.com>
2025-03-22 10:36:10 -0300

    Ajustando redirecionamento após login

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -23,7 +23,7 @@ export async function login(formData: FormData) {
   }
 
   revalidatePath("/", "layout");
-  redirect("/");
+  redirect("/gerar-imagem");
 }
 
 export async function signup(formData: FormData) {
